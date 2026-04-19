f7f29b8e2f044c6c7864d9d6113e920775ec36d7
H4R1B0 <h4r1b0@H4R1B0ui-Macmini.local>
2026-04-19 10:53:40 +0900

    feat(loader): bookmarklet source that injects main bundle

---
+++ loader.js
@@ -0,0 +1,8 @@
+(() => {
+  if (window.__cgvBot2026__ && typeof window.__cgvBot2026__.teardown === 'function') {
+    window.__cgvBot2026__.teardown();
+  }
+  const s = document.createElement('script');
+  s.src = 'https://h4r1b0.github.io/cgv-bot-2026/cgv-bot.js?v=' + Date.now();
+  document.body.appendChild(s);
+})();
